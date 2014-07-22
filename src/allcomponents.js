8559164e8d5f1a4249593f5058d6806f2d15ff68
sdierauf <sdierauf@gmail.com>
2014-07-22 14:33:43 -0700

    all done with the first thingy

--- src/allcomponents.js
+++ src/allcomponents.js
@@ -37,12 +37,10 @@ var CommentForm = React.createClass({
     handleSubmit: function() {
         var author = this.refs.author.getDOMNode().value.trim();
         var text = this.refs.text.getDOMNode().value.trim();
-        if(!text || !author) {
-            return false;
-        }
-        //TODO: send request to the server
+        this.props.onCommentSubmit({author: author, text: text});
         this.refs.author.getDOMNode().value = '';
         this.refs.text.getDOMNode().value = '';
+        return false;
     },
     render: function() {
         return (
@@ -75,18 +73,35 @@ var CommentBox = React.createClass({
         this.loadCommentsFromServer();
         setInterval(this.loadCommentsFromServer, this.props.pollInterval);
     },
+    handleCommentSubmit: function(comment) {
+        var comments = this.state.data;
+        var newComments = comments.concat([comment]);
+        this.setState({data: newComments});
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'POST',
+            data: comment,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
     render: function() {
         return (
             <div className="commentBox">
                 Hello World! I am a CommentBox.
                 <CommentList data={this.state.data}/>
-                <CommentForm />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
             </div>
         );
     }
 });
 
 React.renderComponent(
-    <CommentBox url="fake-api/comments.json" pollInterval={2000} />,
+    <CommentBox url="comments.json" pollInterval={2000} />,
     document.getElementById('content')
 );
