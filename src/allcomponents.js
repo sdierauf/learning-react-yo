e07f5172f3ac3a67fb173551edf7a62df12c0fd6
sdierauf <sdierauf@gmail.com>
2014-07-22 14:19:02 -0700

    using external json, setting up submit form

--- src/allcomponents.js
+++ src/allcomponents.js
@@ -2,11 +2,6 @@
 
 var converter = new Showdown.converter
 
-var data = [
-    {author: "Stefan Dierauf", text: "fuck script **tags** *yo*"}
-]
-
-
 var Comment = React.createClass({
     render: function() {
         var rawMarkup = converter.makeHtml(this.props.children.toString());
@@ -39,21 +34,52 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+    handleSubmit: function() {
+        var author = this.refs.author.getDOMNode().value.trim();
+        var text = this.refs.text.getDOMNode().value.trim();
+        if(!text || !author) {
+            return false;
+        }
+        //TODO: send request to the server
+        this.refs.author.getDOMNode().value = '';
+        this.refs.text.getDOMNode().value = '';
+    },
     render: function() {
         return (
-            <div className="commentForm">
-                Hello world, I am a commentform
-            </div>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Your name" ref="author" />
+                <input type="text" placeholder="Say something..." ref="text" />
+                <input type="submit" value="Post" />
+            </form>
         );
     }
 });
 
 var CommentBox = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    loadCommentsFromServer: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    componentDidMount: function() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
     render: function() {
         return (
             <div className="commentBox">
                 Hello World! I am a CommentBox.
-                <CommentList data={this.props.data}/>
+                <CommentList data={this.state.data}/>
                 <CommentForm />
             </div>
         );
@@ -61,6 +87,6 @@ var CommentBox = React.createClass({
 });
 
 React.renderComponent(
-    <CommentBox data={data} />,
+    <CommentBox url="fake-api/comments.json" pollInterval={2000} />,
     document.getElementById('content')
 );
